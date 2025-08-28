8bbf05a0f15bc241395de013249e4acd4e18c01b
Aman <amankaushik12.ji@gmail.com>
2025-08-28 15:51:27 +0530

    Add files via upload

---
+++ q7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int a,b ;
+
+    printf("enter 1st number :");
+    scanf("%d", &a);
+
+    printf("enter 2nd number :");
+    scanf("%d", &b);
+
+    a = a+b ;
+    b = a-b ;
+    a = a-b ;
+     
+    printf("value of  a and b is %d%d ", a,b );
+
+    return 0;
+}
