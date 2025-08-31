2cc6098405c9f9e52fbd31c87e67f18e75610b29
Aman <amankaushik12.ji@gmail.com>
2025-08-31 18:29:31 +0530

    Add files via upload

---
+++ Q12.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main() {
+    int n ;
+    printf("the value of n :");
+    scanf("%d", &n);
+ 
+    if (n>0){
+        printf("n > 0\n");
+    }
+
+    else if (n == 0) {
+            printf("n = 0\n ");
+
+
+        }
+    
+    else {
+printf ("n<0\n");
+
+    }
+
+    }
