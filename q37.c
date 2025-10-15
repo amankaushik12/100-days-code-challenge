c72560c49368ed53359f70f3d1b480a647324621
Aman <amankaushik12.ji@gmail.com>
2025-10-15 16:17:06 +0530

    Add files via upload

---
+++ q37.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, x, y, hcf, lcm;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    x = a;
+    y = b;
+
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    hcf = a;
+
+    // LCM formula: (num1 * num2) / HCF
+    lcm = (x * y) / hcf;
+
+    printf("LCM of %d and %d is: %d\n", x, y, lcm);
+
+    return 0;
+}
